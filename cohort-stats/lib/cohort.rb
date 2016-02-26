f9058474e36890137550e13671d87f0e9dc23bf9
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-02-26 09:54:17 -0500

    Systems Check 1
    Meets Expectations submission

---
+++ cohort-stats/lib/cohort.rb
@@ -0,0 +1,33 @@
+class Cohort
+  attr_reader :title, :start_date, :end_date
+  attr_accessor :system_checks, :students
+
+  def initialize(title, start_date, end_date)
+    @title = title
+    @start_date = start_date
+    @end_date = end_date
+    @system_checks = []
+    @students = []
+  end
+
+  def career_day
+    @end_date + 4
+  end
+
+  def enroll(student)
+    @students << student
+  end
+
+  def assign(system_check)
+    @system_checks << system_check
+  end
+
+  def roster
+    output = "\nCohort Roster: #{title}\n"
+    output += "---------------------------"
+    @students.each do |student|
+      output += "#{student.name} #{student.email}\n"
+    end
+    output
+  end
+end
---
+++ cohort-stats/lib/system_check.rb
@@ -0,0 +1,13 @@
+class SystemCheck
+  attr_accessor :submissions
+
+  def initialize(name, due_date)
+    @name = name
+    @due_date = due_date
+    @submissions = []
+  end
+
+  def add_submission(submission)
+    @submissions << submission
+  end
+end
