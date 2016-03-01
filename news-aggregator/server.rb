4ce67f95f40066437adf7115f88ec073ac6ad5bd
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-03-01 13:14:14 -0500

    Figured out basics
    Need to reformat with html/css

--- news-aggregator/server.rb
+++ news-aggregator/server.rb
@@ -2,15 +2,22 @@ require 'sinatra'
 require 'pry'
 
 get '/articles' do
-  @articles = File.readlines('ingredients.txt')
+  @articles = File.readlines('articles.txt')
   erb :index
 end
 
+get '/articles/new' do
+  erb :show
+end
+
 post '/articles/new' do
-  article = params['article']
+  title = params['title']
+  url = params['url']
+  description = params['description']
+
 
   File.open('articles.txt', 'a') do |file|
-    file.puts(article)
+    file.puts(title, url, description)
   end
 
   redirect '/articles'
