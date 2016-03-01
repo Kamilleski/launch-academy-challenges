ca149ed0f066bc568e9b9f103c2ed9a3c9fd8543
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-03-01 15:52:29 -0500

    Finished required elements

--- news-aggregator/server.rb
+++ news-aggregator/server.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
-require 'pry'
+require 'csv'
+
+get '/' do
+  redirect '/articles'
+end
 
 get '/articles' do
-  @articles = File.readlines('articles.txt')
+  @articles = CSV.readlines('articles.csv')
   erb :index
 end
 
@@ -11,13 +15,13 @@ get '/articles/new' do
 end
 
 post '/articles/new' do
-  title = params['title']
-  url = params['url']
-  description = params['description']
+  title = params[:title]
+  url = params[:url]
+  description = params[:description]
 
 
-  File.open('articles.txt', 'a') do |file|
-    file.puts(title, url, description)
+  CSV.open('articles.csv', 'a') do |csv|
+    csv << [title, url, description]
   end
 
   redirect '/articles'
