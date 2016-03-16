db4bd77f2721af0286833d70a23ef390c23ac48a
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-03-16 18:56:52 -0400

    completed migrations marathon

---
+++ migrations-marathon-master/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Example:
+#
+#   Person.create(first_name: 'Eric', last_name: 'Kelly')
+
+
+Category.create(name: "Fiction")
+Category.create(name: "Horror")
+Category.create(name: "Sci-Fi")
+Category.create(name: "Fantasy")
+Category.create(name: "Non-Fiction")
