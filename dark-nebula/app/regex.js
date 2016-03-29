c2e6ad8157959663baf8f5e4526a5bda3f13368d
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-03-29 17:40:01 -0400

    finished regex

--- dark-nebula/app/regex.js
+++ dark-nebula/app/regex.js
@@ -2,28 +2,49 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
   containsNumber : function(str) {
-    if (str.includes("[0-9]")) {
+    if (str.match(/\d/) !== null) {
       return true
     }
     else {
       return false
     }
-
   },
 
   containsRepeatingLetter : function(str) {
-
+    if (str.match(/([a-z])\1/i) !== null) {
+      return true
+    }
+    else {
+      return false
+    }
   },
 
   endsWithVowel : function(str) {
-
+    if (str.match(/[aeiou]$/i) !== null) {
+      return true
+    }
+    else {
+      return false
+    }
   },
 
   captureThreeNumbers : function(str) {
-
+    kamille = str.match(/\d{3}/)
+    if (kamille !== null) {
+      return kamille[0]
+    }
+    else {
+      return false
+    }
   },
 
   matchesPattern : function(str) {
+    if (str.match(/^\d{3}-\d{3}-\d{4}$/) !== null) {
+      return true
+    }
+    else {
+      return false
+    }
 
   },
 
