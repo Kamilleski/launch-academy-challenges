4e0207003a597a1108b3746eacde9d1166691c8a
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-03-30 22:00:57 -0400

    Finished afternoon challenges

--- launchbook-jquery/solutions.js
+++ launchbook-jquery/solutions.js
@@ -1,31 +1,50 @@
 // Exercise: Find the top navbar by query for the element type, which is <nav>.
+$('nav');
 
 // Exercise: Find the sidebar on the right by using it's id.
+$('#sidebar-left');
 
 // Exercise: Find the "Pages" and "Groups" sections of the sidebar by using their class.
+$('.pages');
+$('.groups');
 
 // Exercise: Find all of the comments on the page.
+$('.panel > .media');
 
 // Exercise: Find the first comment on the page.
+$('.panel > .media').first()
 
 // Exercise: Find the last comment on the page.
+$('.panel > .media').last()
+
 
 // Exercise: Find the fourth comment on the page.
+$($('.panel > .media')[3]);
 
 // Exercise: Find one of the ads in the sidebar and use .hide() to hide it.
+$('.ad-slot').first().hide();
 
 // Exercise: Use .show() to make the ad that you hid in the previous step visible again.
+$('ads').first().show();
 
 // Exercise: Use .toggle() to toggle the display of the nav bar at the top of the page.
+$('nav').toggle();
 
 // Exercise: Use .attr() to change src attribute of one of the ads in the sidebar. Here's an image source if you need one: http://placebear.com/200/300.
+var $firstAd = $('.ads img').first();
+$firstAd.attr('src', 'http://placebear.com/200/300');
 
 // Exercise: Find Sam's post and change it's text to something incredible.
+$($('.media-body > p')[8]).text('something incredible');
 
 // Exercise: Find the first post and use .addClass() to add the .post-liked class to it.
+var $firstPost = $('.post').first();
+$firstPost.addClass('post-liked');
 
 // Exercise: Find the second post and use .removeClass() to remove the .post-liked class.
+var $secondPost = $($('.post')[1]);
+$secondPost.removeClass('post-liked');
 
 // Exercise: Find any post and use .toggleClass() to toggle the .post-liked class.
 
-
+$secondPost.toggleClass('post-liked');
--- launchbook-js/solutions.js
+++ launchbook-js/solutions.js
@@ -1,25 +1,51 @@
 // Exercise: Find the top navbar by query for the element type, which is <nav>.
+document.querySelector("nav");
 
 // Exercise: Find the sidebar on the right by using it's id.
+document.getElementById('sidebar-left');
 
 // Exercise: Find the "Pages" and "Groups" sections of the sidebar by using their class.
+document.getElementsByClassName('pages');
+document.getElementsByClassName('groups');
 
 // Exercise: Find all of the comments on the page.
+document.getElementsByClassName('comments');
+
 
 // Exercise: Find the first comment on the page.
+document.getElementsByClassName('comments')[0];
+
 
 // Exercise: Find the last comment on the page.
+var comments = document.getElementsByClassName('comments');
+comments[comments.length - 1];
 
 // Exercise: Find the fourth comment on the page.
+document.getElementsByClassName('comments')[3];
 
 // Exercise: Find one of the ads in the sidebar and hide them.
+var ad1 = document.getElementsByClassName('ads')[0];
+ad1.style.visibility = "hidden";
 
 // Exercise: Set the visibility to the form that you hid in the previous step to make it visible again.
+ad1.style.visibility = "visible";
 
 // Exercise: Use setAttribute to change src attribute of one of the ads in the sidebar.
+var adSrc = ad1.getElementsByTagName('src')[0];
+adSrc.setAttribute('img', 'http://placebear.com/200/300')
 
 // Exercise: Find Sam's post and change it's text to something incredible.
 
+var sam = document.getElementsByClassName('post media')[3];
+var samBody = sam.getElementsByClassName('media-body')[0];
+var samText = samBody.getElementsByTagName('p')[0];
+samText.innerHTML = "something incredible"
+
 // Exercise: Find the first post and add the .post-liked class to it.
+var post1 = document.getElementsByClassName('post media')[0];
+post1.className = post1.className + " post-liked";
+
 
 // Exercise: Find the second post and remove the .post-liked class.
+var post2 = document.getElementsByClassName('post media')[1];
+post2.className = "post media";
