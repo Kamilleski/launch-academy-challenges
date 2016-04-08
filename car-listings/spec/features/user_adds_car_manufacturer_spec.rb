96239d2b4eeaafd6ec8d3268507cde367b4f31e7
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-04-08 10:31:12 -0400

    'Add Manufacturer' feature implemented

--- car-listings/spec/features/user_adds_car_manufacturer_spec.rb
+++ car-listings/spec/features/user_adds_car_manufacturer_spec.rb
@@ -1,5 +1,22 @@
-# As a car salesperson
-# I want to record a car manufacturer
-# So that I can keep track of the types of cars found in the lot
-
 require 'rails_helper'
+
+feature 'salesperson records car manufacturer' do
+  scenario 'salesperson adds name and country' do
+    visit new_manufacturer_path
+
+    fill_in "Name", with: "Subaru"
+    fill_in "Country", with: "Japan"
+    click_on "Submit"
+
+    expect(page).to have_content("Manufacturers")
+    expect(page).to have_content("Subaru")
+  end
+
+  scenario 'salesperson does not add all fields' do
+    visit new_manufacturer_path
+
+    click_on "Submit"
+    expect(page).to have_content("Name can't be blank.")
+    expect(page).to have_content("New Manufacturer Form")
+  end
+end
