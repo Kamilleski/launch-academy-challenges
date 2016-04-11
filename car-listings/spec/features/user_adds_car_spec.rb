dbf2c63dfbf0a1ced7d08204351c799218811299
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-04-11 19:17:31 -0400

    Initial Commit

--- car-listings/spec/features/user_adds_car_spec.rb
+++ car-listings/spec/features/user_adds_car_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 feature 'user adds a car' do
   scenario 'user successfully adds a car' do
-
+    Manufacturer.create(name: "Ferrari", country: "Italy")
     visit new_car_path
-
     save_and_open_page
-    select "Tesla", from: "car_manufacturer_id"
+
+    select "Ferrari", from: "Manufacturer"
     fill_in "Color", with: "Baby Blue"
     fill_in "Year", with: "2016"
     fill_in "Mileage", with: "0"
@@ -17,6 +17,8 @@ feature 'user adds a car' do
   end
 
   scenario 'user adds car that is too old' do
+    visit new_car_path
+
     fill_in "Color", with: "Baby Blue"
     fill_in "Year", with: "1800"
     fill_in "Mileage", with: "0"
